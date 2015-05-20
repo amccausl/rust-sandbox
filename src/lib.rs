c430ea702b07e6c1ca1e16263ee14389342441d6
Alex McCausland <alex.mccausland@gmail.com>
2015-05-20 07:25:36 -0400

    Add katakana romanization, some cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 
 /// Hepburn romanization for input string
 /// @todo return an iterator with result
-/// @todo add katakana
 /// @todo unicode unwrap for range comparison, byte detection for digraphs
 /// @todo benchmarking
+/// @todo add handling for 一
 pub fn romanize( input: &str ) -> String {
     /*
     for b in input.as_bytes() {
@@ -18,11 +18,11 @@ pub fn romanize( input: &str ) -> String {
 
         match it.next() {
             // Hiragana
-            Some('あ') => result.push_str("a"),
-            Some('い') => result.push_str("i"),
-            Some('う') => result.push_str("u"),
-            Some('え') => result.push_str("e"),
-            Some('お') => result.push_str("o"),
+            Some('あ') => result.push('a'),
+            Some('い') => result.push('i'),
+            Some('う') => result.push('u'),
+            Some('え') => result.push('e'),
+            Some('お') => result.push('o'),
 
             Some('か') => result.push_str("ka"),
             Some('き') => match it.peek() {
@@ -126,7 +126,7 @@ pub fn romanize( input: &str ) -> String {
 
             Some('だ') => result.push_str("da"),
             Some('ぢ') => result.push_str("ji"),
-            Some('づ') => result.push_str("zu"),
+            Some('づ') => result.push_str("dzu"),
             Some('で') => result.push_str("de"),
             Some('ど') => result.push_str("do"),
 
@@ -166,7 +166,7 @@ pub fn romanize( input: &str ) -> String {
                 },
                 _ => result.push_str("hi"),
             },
-            Some('ふ') => result.push_str("hu"),
+            Some('ふ') => result.push_str("fu"),
             Some('へ') => result.push_str("he"),
             Some('ほ') => result.push_str("ho"),
 
@@ -255,9 +255,267 @@ pub fn romanize( input: &str ) -> String {
             Some('ろ') => result.push_str("ro"),
 
             Some('わ') => result.push_str("wa"),
-            Some('を') => result.push_str("wo"),
+            Some('を') => result.push('o'),
 
             Some('ん') => result.push('n'),
+
+            // Katakana
+            Some('ア') => result.push('a'),
+            Some('イ') => result.push('i'),
+            Some('ウ') => result.push('u'),
+            Some('エ') => result.push('e'),
+            Some('オ') => result.push('o'),
+
+            Some('カ') => result.push_str("ka"),
+            Some('キ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("kya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("kyu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("kyo");
+                },
+                _ => result.push_str("ki"),
+            },
+            Some('ク') => result.push_str("ku"),
+            Some('ケ') => result.push_str("ke"),
+            Some('コ') => result.push_str("ko"),
+
+            Some('ガ') => result.push_str("ga"),
+            Some('ギ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("gya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("gyu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("gyo");
+                },
+                _ => result.push_str("gi"),
+            },
+            Some('グ') => result.push_str("gu"),
+            Some('ゲ') => result.push_str("ge"),
+            Some('ゴ') => result.push_str("go"),
+
+            Some('サ') => result.push_str("sa"),
+            Some('シ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("sha");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("shu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("sho");
+                },
+                _ => result.push_str("shi"),
+            },
+            Some('ス') => result.push_str("su"),
+            Some('セ') => result.push_str("se"),
+            Some('ソ') => result.push_str("so"),
+
+            Some('ザ') => result.push_str("za"),
+            Some('ジ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("ja");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("ju");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("jo");
+                },
+                _ => result.push_str("zi"),
+            },
+            Some('ズ') => result.push_str("zu"),
+            Some('ゼ') => result.push_str("ze"),
+            Some('ゾ') => result.push_str("zo"),
+
+            Some('タ') => result.push_str("ta"),
+            Some('チ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("cha");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("chu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("cho");
+                },
+                _ => result.push_str("chi"),
+            },
+            Some('ツ') => result.push_str("tsu"),
+            Some('テ') => result.push_str("te"),
+            Some('ト') => result.push_str("to"),
+
+            Some('ダ') => result.push_str("da"),
+            Some('ヂ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("ja");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("ju");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("jo");
+                },
+                _ => result.push_str("ji"),
+            },
+            Some('ヅ') => result.push_str("zu"),
+            Some('デ') => result.push_str("de"),
+            Some('ド') => result.push_str("do"),
+
+            Some('ナ') => result.push_str("na"),
+            Some('ニ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("nya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("nyu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("nyo");
+                },
+                _ => result.push_str("ni"),
+            },
+            Some('ヌ') => result.push_str("nu"),
+            Some('ネ') => result.push_str("ne"),
+            Some('ノ') => result.push_str("no"),
+
+            Some('ハ') => result.push_str("ha"),
+            Some('ヒ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("hya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("hyu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("hyo");
+                },
+                _ => result.push_str("hi"),
+            },
+            Some('フ') => result.push_str("fu"),
+            Some('ヘ') => result.push_str("he"),
+            Some('ホ') => result.push_str("ho"),
+
+            Some('バ') => result.push_str("ba"),
+            Some('ビ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("bya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("byu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("byo");
+                },
+                _ => result.push_str("bi"),
+            },
+            Some('ブ') => result.push_str("bu"),
+            Some('ベ') => result.push_str("be"),
+            Some('ボ') => result.push_str("bo"),
+
+            Some('パ') => result.push_str("pa"),
+            Some('ピ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("pya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("pyu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("pyo");
+                },
+                _ => result.push_str("pi"),
+            },
+            Some('プ') => result.push_str("pu"),
+            Some('ペ') => result.push_str("pe"),
+            Some('ポ') => result.push_str("po"),
+
+            Some('マ') => result.push_str("ma"),
+            Some('ミ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("mya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("myu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("myo");
+                },
+                _ => result.push_str("mi"),
+            },
+            Some('ム') => result.push_str("mu"),
+            Some('メ') => result.push_str("me"),
+            Some('モ') => result.push_str("mo"),
+
+            Some('ヤ') => result.push_str("ya"),
+            Some('ユ') => result.push_str("yu"),
+            Some('ヨ') => result.push_str("yo"),
+
+            Some('ラ') => result.push_str("ra"),
+            Some('リ') => match it.peek() {
+                Some(&'ャ') => {
+                    it.next();
+                    result.push_str("rya");
+                },
+                Some(&'ュ') => {
+                    it.next();
+                    result.push_str("ryu");
+                },
+                Some(&'ョ') => {
+                    it.next();
+                    result.push_str("ryo");
+                },
+                _ => result.push_str("ri"),
+            },
+            Some('ル') => result.push_str("ru"),
+            Some('レ') => result.push_str("re"),
+            Some('ロ') => result.push_str("ro"),
+
+            Some('ワ') => result.push_str("wa"),
+            Some('ヰ') => result.push_str("wi"),
+            Some('ヱ') => result.push_str("we"),
+            Some('ヲ') => result.push_str("wo"),
+
+            Some('ン') => result.push('n'),
 
             // Copy other characters unchanged
             Some( value ) => result.push( value ),
@@ -268,17 +526,17 @@ pub fn romanize( input: &str ) -> String {
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
-    fn it_handles_single_phonemes() {
+    fn it_handles_monographs() {
         assert_eq!( "ru", romanize( "る" ) );
     }
 
     #[test]
     fn it_handles_digraphs() {
-        assert_eq!( "kurya", romanize( "くりゃ" ) );
+        assert_eq!( "rya", romanize( "りゃ" ) );
     }
 
     #[test]
@@ -289,5 +547,19 @@ mod test {
     #[test]
     fn it_handles_semi_voiced_mark() {
         assert_eq!( "pa", romanize( "ぱ" ) );
+    }
+
+    #[test]
+    fn it_handles_long_vowel() {
+        assert_eq!( "mā", romanize( "マー" ) )
+    }
+
+    #[test]
+    fn it_romanizes_chinese_loan() {
+        assert_eq!( "mājan", romanize( "マージャン" ) );
+        assert_eq!( "ūroncha", romanize( "ウーロン茶" ) );
+        assert_eq!( "chāhan", romanize( "チャーハン" ) );
+        assert_eq!( "chāshū", romanize( "チャーシュー" ) );
+        assert_eq!( "shūmai", romanize( "シューマイ" ) );
     }
 }
